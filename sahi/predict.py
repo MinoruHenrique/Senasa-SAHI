b98d0a833024c29e3102c0d6674ec46049f8993a
MinoruHenrique <henrique.deaguiar.m@uni.pe>
2023-04-13 20:09:09 -0500

    Modify dirs

--- sahi/predict.py
+++ sahi/predict.py
@@ -427,7 +427,7 @@ def predict(
     # init export directories
     save_dir = Path(increment_path(Path(project), exist_ok=True))  # increment run
     crop_dir = save_dir / "crops"
-    visual_dir = save_dir 
+    visual_dir = save_dir / "outputs"
     visual_with_gt_dir = save_dir / "visuals_with_gt"
     pickle_dir = save_dir / "pickles"
     if not novisual or export_pickle or export_crop or dataset_json_path is not None:
